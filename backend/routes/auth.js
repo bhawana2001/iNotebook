43bf7c0a0a15810fefb9dc654c08585fb5099e76
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-10-18 10:53:55 +0530

    apply validators and create user database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,25 @@
-const express=require('express')
-const router=express.Router()
-const User=require('../models/User')
-
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator')
 
 //Create a User using: POST "/api/auth/" . Doesn't require Auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save()
-    res.send(req.body)
+router.post('/', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('password', 'Password must be at least 8 characters').isLength({ min: 8 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: error.array() })
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+    res.json({error:'Please enter unique value  for email',message:err.message})})
 })
 
-module.exports=router
+module.exports = router

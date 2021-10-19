25b396793178855b65a795a8ccb084157e5856b2
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-10-19 09:25:38 +0530

    create hash password with salt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator')
+const bcrypt=require('bcryptjs');
 
 //Create a User using: POST "/api/auth/createUser" . No login required
 router.post('/createuser', [
@@ -21,11 +22,15 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "Sorry a user with this email already exists" })
         }
+        //create secure password function 
+        const salt=await bcrypt.genSalt(10);  //create a salt
+       const secPass=await bcrypt.hash(req.body.password,salt);
+
         //create a new user
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         })
         res.json(user)
 

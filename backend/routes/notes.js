01f82f4b3b8f3f8647a127ab01e8541e432448e5
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-10-22 18:13:10 +0530

    create endpoint for update a note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -41,4 +41,25 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+//ROUTE : 3 - update a existing notes using: POST "/api/auth/updatenote" . login required
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+
+    const { title, description, tag } = req.body
+    //create a new note object
+    const newNote = {};
+    if (title) { newNote.title = title }
+    if (description) { newNote.description = description }
+    if (tag) { newNote.tag = tag }
+    //fins the note to be updated and update it
+    //if note not found
+    let note = await Note.findById(req.params.id);
+    if (!note) { return res.status(404).send("Not Found") }
+
+    //if user is not correct for note user id
+    if (note.user.toString() != req.user.id) {
+        return res.status(401).send("Not Allowed");
+    }
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+    res.json(note);
+})
 module.exports = router

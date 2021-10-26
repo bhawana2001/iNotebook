254a2f6ff20a69b5ef414ea9a418a4eb11b199f4
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-10-26 18:59:23 +0530

    some additional commits

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
 import { useEffect } from 'react'
 
 
 export const About = () => {
-    const a=useContext(noteContext)
-    useEffect(() => {
-        a.update()
-        // eslint-disable-next-line
-    }, [])
     return (
         <div>
-            This is about {a.state.name}
+            This is about page.
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,20 +3,9 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-    const s1 = {
-        "name": "Bhawana",
-        "class": "5b"
-    }
-    const [state, setstate] = useState(s1)
-  const  update = () => {
-        setTimeout(() => {
-            setstate({
-                "name": "Gaur",
-            })
-        }, 1000)
-    }
+   
     return (
-        <NoteContext.Provider value={{state,update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     )

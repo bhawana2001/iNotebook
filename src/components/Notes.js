1efd07dcb475a049fe5399341661b15e42b5ce94
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-10-29 11:10:34 +0530

    add getNotes function

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,20 +1,23 @@
 import noteContext from "../context/notes/noteContext"
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import NoteItem from "./NoteItem"
 import AddNote from "./AddNote"
 
 const Notes = () => {
     const context = useContext(noteContext)
-    const { notes ,addNote} = context
+    const { notes, getNotes } = context
+    useEffect(() => {
+        getNotes()
+    }, [])
     return (
         <>
-        <AddNote />
-        <div className="row my-3">
-            <h2>Your Notes</h2>
-            {notes.map((note) => {
-                return <NoteItem key={note._id} note={note} />
-            })}
-        </div>
+            <AddNote />
+            <div className="row my-3">
+                <h2>Your Notes</h2>
+                {notes.map((note) => {
+                    return <NoteItem key={note._id} note={note} />
+                })}
+            </div>
         </>
     )
 }
